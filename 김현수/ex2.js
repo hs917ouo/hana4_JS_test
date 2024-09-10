a10cc84c03d5b9a59d3e3b7523c39c80ea599ead
campus2 H015 <h015@alpacoedu.onmicrosoft.com>
2024-09-10 15:54:28 +0900

    not yet ex2.js

--- 김현수/ex2.js
+++ 김현수/ex2.js
@@ -1,4 +1,13 @@
 // range 함수를 작성하세요.
-const range = (start, end, step = start > end ? -1 : 1) => { };
-
+const range = (start, end, step = start > end ? -1 : 1) => {
+  if (!end && s === 0) return [0];
+  if (!end && s > 0) {
+    end = start;
+    start = 1;
+  }
+  if (!end && s < 0) {
+    end = -1;
+  }
+  if (step === 0 || start === end) return [start];
+};
 module.exports = { range };
