2ea6912409bd25f6105e8e5c42af77e14f95892b
campus2 H015 <h015@alpacoedu.onmicrosoft.com>
2024-09-10 19:31:17 +0900

    update ex2.js

--- 김현수/ex2.js
+++ 김현수/ex2.js
@@ -1,13 +1,29 @@
 // range 함수를 작성하세요.
 const range = (start, end, step = start > end ? -1 : 1) => {
-  if (!end && s === 0) return [0];
-  if (!end && s > 0) {
-    end = start;
-    start = 1;
+  if (end === undefined) {
+    if (start === 0) return [0];
+    if (start > 0) {
+      end = start;
+      start = 1;
+    }
+    if (start < 0) {
+      end = -1;
+    }
   }
-  if (!end && s < 0) {
-    end = -1;
-  }
+  if ((start - end) * step > 0) return [];
   if (step === 0 || start === end) return [start];
+
+  const count = Math.abs(Math.trunc((end - start) / step)) + 1;
+  const ret = [start];
+  for (let i = 1; i <= count - 1; i += 1) {
+    ret.push(ret[ret.length - 1] + step);
+  }
+  if (parseInt(step) !== parseFloat(step)) {
+    const floatLen = (parseFloat(step) - parseInt(step)).toString().length - 2;
+    for (i in ret) {
+      ret[i] = parseFloat(ret[i].toFixed(floatLen));
+    }
+  }
+  return ret;
 };
 module.exports = { range };
