d17bde18000388334c52ed8a03f364b1b2d0fcd2
campus2 H015 <h015@alpacoedu.onmicrosoft.com>
2024-09-11 23:49:17 +0900

    not yet ex4

--- FEBasic/김현수/ex4.js
+++ FEBasic/김현수/ex4.js
@@ -1,3 +1,50 @@
-function deepCopy(obj) {}
+const { isMap, isSet, isWeakMap, isWeakSet } = require("util/types");
+
+function isObject(obj) {
+  return obj && typeof obj === "object";
+}
+
+function deepCopy(obj) {
+  if (!isObject(obj)) {
+    return obj;
+  }
+  if (Array.isArray(obj)) {
+    const arr = [];
+    for (a in obj) {
+      arr.push(deepCopy(obj[a]));
+    }
+    return arr;
+  }
+  if (isMap(obj)) {
+    const map = new Map();
+    for (const [key, value] of obj) {
+      const deepKey = deepCopy(key);
+      const deepValue = deepCopy(value);
+      map.set(deepKey, deepValue);
+    }
+    return map;
+  }
+  if (isWeakMap(obj)) {
+    const copy = new WeakMap();
+    obj.set((value, key) => {
+      if (typeof key === "object" && key !== null) {
+        const deepValue = deepCopy(value);
+        copy.set(key, deepValue);
+      }
+    });
+    return copy;
+  }
+  //   if (isMap(obj)) return obj;
+  //   if (isWeakMap(obj)) return obj;
+  if (isSet(obj)) return obj;
+  if (isWeakSet(obj)) return obj;
+
+  const newer = {};
+  for (const k of Reflect.ownKeys(obj)) {
+    newer[k] = deepCopy(obj[k]);
+  }
+
+  return newer;
+}
 
 module.exports = { deepCopy };
--- FEBasic/김현수/ex4.test.js
+++ FEBasic/김현수/ex4.test.js
@@ -1,14 +1,14 @@
-const assert = require('assert');
-const { deepCopy } = require('./ex4');
+const assert = require("assert");
+const { deepCopy } = require("./ex4");
 
 const arr = [1, 2, 3];
-const hong = { id: 1, name: 'Hong', city: 'Busan', dept: 1 };
+const hong = { id: 1, name: "Hong", city: "Busan", dept: 1 };
 
 const kim = {
   nid: 3,
-  addr: 'Pusan',
+  addr: "Pusan",
   arr: [1, 2, 3, { aid: 1 }, [10, 20]],
-  oo: { id: 1, name: 'Hong', addr: { city: 'Seoul' } },
+  oo: { id: 1, name: "Hong", addr: { city: "Seoul" } },
   xx: null,
   yy: function () {
     console.log(this.oo);
@@ -17,24 +17,60 @@ const kim = {
     console.log(this.oo);
   },
   [Symbol()]: 9,
-  [Symbol()]: Symbol('symbol2'),
+  [Symbol()]: Symbol("symbol2"),
   zs: new Set([arr, hong]),
   zws: new WeakSet([arr, hong]),
   zm: new Map([[hong, arr]]),
   zwm: new WeakMap([[hong, arr]]),
 };
 const newKim = deepCopy(kim);
-assert.deepStrictEqual(newKim, kim, 'deepCopy equal fail!');
-newKim.addr = 'Daegu';
-newKim.oo.name = 'Kim';
-assert.notDeepStrictEqual(newKim, kim, 'Not Valid Deep Copy!');
+assert.deepStrictEqual(newKim, kim, "deepCopy equal fail!");
+newKim.addr = "Daegu";
+newKim.oo.name = "Kim";
+assert.notDeepStrictEqual(newKim, kim, "Not Valid Deep Copy!");
 newKim.arr[0] = 100;
 newKim.arr[3].aid = 200;
 newKim.arr[4][1] = 300;
-newKim.oo.addr.city = 'Daejeon';
-assert.notStrictEqual(kim.arr[4][1], newKim.arr[4][1], 'pass2: 다르지 않아요!');
+newKim.oo.addr.city = "Daejeon";
+assert.notStrictEqual(kim.arr[4][1], newKim.arr[4][1], "pass2: 다르지 않아요!");
 assert.notStrictEqual(
   kim.oo.addr.city,
   newKim.oo.addr.city,
-  'Not Pass3: city가 다르지 않아요!'
+  "Not Pass3: city가 다르지 않아요!"
 );
+const obj1 = { a: 1 };
+newKim.zm.set("key", "value");
+assert.notDeepStrictEqual(newKim.zm, kim.zm);
+
+newKim.zwm.set(obj1, "value");
+console.log(kim.zwm);
+assert.notDeepStrictEqual(newKim.zwm, kim.zwm);
+
+const map = new Map();
+const set = new Set();
+const weakMap = new WeakMap();
+const weakSet = new WeakSet();
+
+console.log(map);
+
+const obj2 = { b: 2 };
+
+map.set("key1", obj1);
+set.add(obj2);
+weakMap.set(obj1, obj2);
+weakSet.add(obj1);
+
+console.log(map);
+
+const deepCopiedMap = deepCopy(map);
+
+console.log(deepCopiedMap);
+deepCopiedMap.set("key1", obj2);
+const deepCopiedSet = deepCopy(set);
+const deepCopiedWeakMap = deepCopy(weakMap);
+const deepCopiedWeakSet = deepCopy(weakSet);
+
+assert.notStrictEqual(map, deepCopiedMap);
+assert.deepStrictEqual(set, deepCopiedSet);
+// assert.deepStrictEqual(weakMap, deepCopiedWeakMap);
+// assert.deepStrictEqual(weakSet, deepCopiedWeakSet);
