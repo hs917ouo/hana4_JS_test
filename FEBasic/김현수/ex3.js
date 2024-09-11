1607a365965c92545ebddcfab6fbfbef41e5eefe
campus2 H015 <h015@alpacoedu.onmicrosoft.com>
2024-09-11 15:24:21 +0900

    update ex3.js

--- FEBasic/김현수/ex3.js
+++ FEBasic/김현수/ex3.js
@@ -1,3 +1,10 @@
-Array.prototype.sortBy = function (sortProp = '') {
+Array.prototype.sortBy = function (sortProp = "") {
+  const arr = sortProp.split(",");
+  arr.reverse();
+  for (prop of arr) {
+    const [key, direction = "asc"] = prop?.split(":");
+    const dir = direction.toLowerCase() === "desc" ? -1 : 1;
+    this.sort((a, b) => (a[key] > b[key] ? dir : -dir));
+  }
   return this;
 };
