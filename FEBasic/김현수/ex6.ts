cb6b319a0cc5b4b4207ace4acd1a5f5e59025098
CzDgB <201801223@hufs.ac.kr>
2024-09-13 03:12:22 +0900

    complete ex6

--- FEBasic/김현수/ex6.ts
+++ FEBasic/김현수/ex6.ts
@@ -1,4 +1,32 @@
 export const randTime = <T>(val: T): Promise<T> =>
-  new Promise(resolve => setTimeout(resolve, Math.random() * 1000, val));
+  new Promise((resolve) => setTimeout(resolve, Math.random() * 1000, val));
 
-export function promiseAllSettled<T>(promises: Promise<T>[]) {}
+export function promiseAllSettled<T>(
+  promises: Promise<T>[],
+): Promise<
+  ({ status: "fulfilled"; value: T } | { status: "rejected"; reason: any })[]
+> {
+  return new Promise((resolve) => {
+    const results: (
+      | { status: "fulfilled"; value: T }
+      | { status: "rejected"; reason: any }
+    )[] = [];
+    let completedPromises = 0;
+
+    promises.forEach((promise, index) => {
+      promise
+        .then((value) => {
+          results[index] = { status: "fulfilled", value };
+        })
+        .catch((reason) => {
+          results[index] = { status: "rejected", reason };
+        })
+        .finally(() => {
+          completedPromises += 1;
+          if (completedPromises === promises.length) {
+            resolve(results);
+          }
+        });
+    });
+  });
+}
