f2d056631ff51ec4ac1003240a65b57ef99a703e
CzDgB <201801223@hufs.ac.kr>
2024-09-12 22:25:25 +0900

    complete ex9

--- FEBasic/김현수/ex9.js
+++ FEBasic/김현수/ex9.js
@@ -5,6 +5,26 @@ const MENU = {
   탕슉: { price: 25000, taxfree: 1 },
 };
 
+const print = {
+  tableNo(num) {
+    console.log(`Table. ${num}`);
+  },
+  dish(food) {
+    console.log(`* ${food}`);
+  },
+  doubleLine() {
+    console.log("=====================");
+  },
+  singleLine() {
+    console.log("---------------------");
+  },
+  price([label, unit], price) {
+    console.log(
+      `${label}`,
+      `${price.toLocaleString()}원`.padStart(LABEL_SIZE, " "),
+    );
+  },
+};
 function bill(tableNo) {
   const total = [];
   let totalSum = 0;
@@ -19,40 +39,22 @@ function bill(tableNo) {
         : "";
     },
     printBill() {
-      printTableNo(tableNo);
-      printDoubleLine();
-      for (food of total) {
-        printFood(food[0]);
-        printPrice`공급가액:${food[1].price}`;
-        printPrice`부가세액:${
+      print.tableNo(tableNo);
+      print.doubleLine();
+      for (let food of total) {
+        print.dish(food[0]);
+        print.price`공급가액:${food[1].price}`;
+        print.price`부가세액:${
           food[1].taxfree === 1 ? 0 : taxCalculate(food[1].price)
         }`;
-        printSingleLine();
+        print.singleLine();
       }
-      printPrice`주문합계:${totalSum}`;
-      printPrice`주문합계:${totalTax}`;
-      printDoubleLine();
+      print.price`주문합계:${totalSum}`;
+      print.price`주문합계:${totalTax}`;
+      print.doubleLine();
       console.log();
     },
   };
-}
-function printTableNo(tableNo) {
-  console.log(`Table. ${tableNo}`);
-}
-function printDoubleLine() {
-  console.log("=====================");
-}
-function printSingleLine() {
-  console.log("---------------------");
-}
-function printFood(food) {
-  console.log(`* ${food}`);
-}
-function printPrice([label, unit], price) {
-  console.log(
-    `${label}`,
-    `${price.toLocaleString()}원`.padStart(LABEL_SIZE, " ")
-  );
 }
 
 const table1 = bill(1);
