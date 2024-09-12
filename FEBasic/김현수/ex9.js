25f6d7a6387fe4e733ba11cc2f0c30cda3b832d4
campus2 H015 <h015@alpacoedu.onmicrosoft.com>
2024-09-12 18:53:03 +0900

    not yet ex9

--- FEBasic/김현수/ex9.js
+++ FEBasic/김현수/ex9.js
@@ -4,19 +4,55 @@ const MENU = {
   탕슉: { price: 25000, taxfree: 1 },
 };
 
-function bill(tableNo) {}
+function bill(tableNo) {
+  const total = [];
+  let sum = 0;
+  let tax = 0;
+  return {
+    order(item) {
+      MENU[item] ? total.push([item, MENU[item]]) : "";
+      MENU[item] ? (sum += MENU[item].price) : "";
+      MENU[item] && MENU[item].taxfree !== 1
+        ? (tax += Math.round(MENU[item].price / 11))
+        : "";
+    },
+    printBill() {
+      console.log(`Table. ${tableNo}`);
+      console.log("=====================");
+      for (food of total) {
+        console.log(`* ${food[0]}`);
+        console.log(
+          `공급가액:`,
+          `${food[1].price.toLocaleString()}원`.padStart(10, " ")
+        );
+        console.log(
+          `부가세액:`,
+          `${
+            food[1].taxfree === 1
+              ? 0
+              : Math.round(food[1].price / 11).toLocaleString()
+          }원`.padStart(10, " ")
+        );
+        console.log("---------------------");
+      }
+      console.log(`주문합계:`, `${sum.toLocaleString()}원`.padStart(10, " "));
+      console.log(`주문합계:`, `${tax.toLocaleString()}원`.padStart(10, " "));
+      console.log(`=====================\n`);
+    },
+  };
+}
 
 const table1 = bill(1);
-table1.order('짜장');
-table1.order('짬뽕');
+table1.order("짜장");
+table1.order("짬뽕");
 table1.printBill();
 
 const table2 = bill(2);
-table2.order('짜장');
+table2.order("짜장");
 table2.printBill();
 
-table1.order('탕슉');
+table1.order("탕슉");
 table1.printBill();
 
-table2.order('짬뽕');
+table2.order("짬뽕");
 table2.printBill();
